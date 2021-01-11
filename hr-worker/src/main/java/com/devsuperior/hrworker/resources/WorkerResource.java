955668a33ca604e80fe8da7c882ede72c9d9379a
MatheusRamos90 <matheus.hrs@live.com>
2021-01-11 19:05:04 -0300

    hr-user configs dev profile

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -2,11 +2,8 @@ package com.devsuperior.hrworker.resources;
 
 import java.util.List;
 
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,20 +18,16 @@ import com.devsuperior.hrworker.repositories.WorkerRepository;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 	
-	private static Logger logger = org.slf4j.LoggerFactory.getLogger(WorkerResource.class);
-	
-	@Value("${test.config}")
-	private String testConfig;
-	
-	@Autowired
-	private Environment env;
+//	private static Logger logger = org.slf4j.LoggerFactory.getLogger(WorkerResource.class);
+//	
+//	@Autowired
+//	private Environment env;
  
 	@Autowired
 	private WorkerRepository repository;
 	
 	@GetMapping(value = "/configs")
 	public ResponseEntity<Void> findConfigs() {
-		logger.info("CONFIG = " + testConfig);
 		return ResponseEntity.noContent().build();		
 	}
 	
@@ -46,16 +39,7 @@ public class WorkerResource {
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Worker> findById(@PathVariable Long id) {
-		
-//		try {
-//			Thread.sleep(3000l);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		
-		logger.info("PORT = " + env.getProperty("local.server.port"));
+	public ResponseEntity<Worker> findById(@PathVariable Long id) {		
 		Worker worker = repository.findById(id).get();
 		return ResponseEntity.ok(worker);
 		
