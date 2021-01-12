dfffdfde26053dd8fdfe1a8d32b57d4132b955c9
MatheusRamos90 <matheus.hrs@live.com>
2021-01-12 12:06:13 -0300

    Add Dockerfile into projects and rename hosts for used containers

--- hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
@@ -1,29 +1,15 @@
 package com.devsuperior.hrconfigserver;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class HrConfigServerApplication implements CommandLineRunner {
-
-	@Value("${spring.cloud.config.server.git.username}")
-	private String username;
+public class HrConfigServerApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		/**
-		 * O valor abaixo foi pego do 'application.properties', cujo mesmo descreve as variáveis de ambiente do sistema operacional, 
-		 * mas pode ser utilizado através das environments da IDE. 
-		 * */
-//		System.out.println("USERNAME = " + username);
 	}
 
 }
